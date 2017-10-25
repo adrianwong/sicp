b1f7550fd7a5c76011a644b775362bfa605cc9b3
Adrian Wong <adrianwjw@gmail.com>
2017-10-25 22:37:54 +1100

    add: 1.44.scm

---
+++ ch_1/1.44.scm
@@ -0,0 +1,15 @@
+(define (repeated f n)
+  (if (= n 1)
+      (lambda (x) (f x))
+      (compose (repeated f (- n 1)) f)))
+
+(define dx 0.00001)
+
+(define (smooth f)
+  (lambda (x) (/ (+ (f (- x dx))
+                    (f x)
+                    (f (+ x dx)))
+                 3.0)))
+
+(define (n-fold-smooth f n x)
+  ((repeated (smooth f) n) x))
