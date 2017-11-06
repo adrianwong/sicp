44368c219a465848ae5cea171b237020857be623
Adrian Wong <adrianwjw@gmail.com>
2017-11-06 15:31:09 +1100

    add: 2.15.scm

---
+++ ch_2/2.15.scm
@@ -0,0 +1,13 @@
+; The formulas for `par1` and `par2` are algebraically
+; identical. 
+
+; (1/(1/R1 + 1/R2))
+; = (1/(1/R1 + 1/R2)) * (R1/R1) * (R2/R2)
+; = (R1 * R2)/(R1 + R2)
+
+; However, as can be seen from the previous exercise, dividing
+; an interval by itself does not equal 1, thus transforming
+; one equation into another introduces errors
+
+; Therefore, Eva Lu Ator is correct. If we avoid repeating variables
+; that represent uncertain numbers, we produce tighter error bounds
