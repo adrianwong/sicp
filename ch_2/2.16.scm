ed527173d77f9143c8f7cee7d16b0e97e2211da4
Adrian Wong <adrianwjw@gmail.com>
2017-11-06 15:31:14 +1100

    add: 2.16.scm

---
+++ ch_2/2.16.scm
@@ -0,0 +1,10 @@
+; From Wikipedia:
+; https://en.wikipedia.org/wiki/Interval_arithmetic#Dependency_problem
+
+; "In general, it can be shown that the exact range of values can
+; be achieved, if each variable appears only once and if f is
+; continuous inside the box. However, not every function can be
+; rewritten this way."
+
+; So, no. Not unless a function can be rewritten to avoid
+; repetition of variables
