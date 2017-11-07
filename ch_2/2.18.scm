caf132d7c12617e8d22781ab89791b4188eef48b
Adrian Wong <adrianwjw@gmail.com>
2017-11-07 15:49:54 +1100

    fix: Add error-checking for empty list. Add iterative solution

--- ch_2/2.18.scm
+++ ch_2/2.18.scm
@@ -1,5 +1,11 @@
 (define (reverse l)
-  (let ((next (cdr l)))
-    (if (null? next)
-        l
-        (append (reverse next) (cons (car l) nil)))))
+  (if (null? l)
+      nil
+      (append (reverse (cdr l)) (list (car l)))))
+
+(define (reverse lst)
+  (define (iter lst revlst)
+    (if (null? lst)
+        revlst
+        (iter (cdr lst) (cons (car lst) revlst))))
+  (iter lst nil))
