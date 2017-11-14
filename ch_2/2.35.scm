179a68edb29c3882a7a9981f81a126ea654521b7
Adrian Wong <adrianwjw@gmail.com>
2017-11-14 22:47:09 +1100

    add: 2.35.scm

---
+++ ch_2/2.35.scm
@@ -0,0 +1,8 @@
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x) 1) (enumerate-tree t))))
