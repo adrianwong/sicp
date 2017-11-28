85437e74d1a92f63e37bf4e3467b3c29d0ae9a34
Adrian Wong <adrianwjw@gmail.com>
2017-11-28 23:28:31 +1100

    add: 2.52.scm

---
+++ ch_2/2.52.scm
@@ -0,0 +1,17 @@
+; (a) Didn't put a smile on that face
+
+; (b)
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1)))
+            (corner (corner-split painter (- n 1))))
+          (beside (below painter up)
+                  (below right corner)))))
+
+; (c)
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-vert rotate180
+                                  identity flip-horiz)))
+    (combine4 (corner-split painter n))))
