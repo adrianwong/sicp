191565424d2bc7c5a0ac9e539c6d80500a661a6e
Adrian Wong <adrianwjw@gmail.com>
2017-11-28 22:18:22 +1100

    add: 2.50.scm

---
+++ ch_2/2.50.scm
@@ -0,0 +1,24 @@
+#lang sicp
+(#%require sicp-pict)
+
+(define (flip-horiz painter)
+  ((transform-painter (make-vect 1.0 0.0)
+                      (make-vect 0.0 0.0)
+                      (make-vect 1.0 1.0))
+   painter))
+
+(define (rotate180 painter)
+  ((transform-painter (make-vect 1.0 1.0)
+                      (make-vect 0.0 1.0)
+                      (make-vect 1.0 0.0))
+   painter))
+
+(define (rotate270 painter)
+  ((transform-painter (make-vect 0.0 1.0)
+                      (make-vect 0.0 0.0)
+                      (make-vect 1.0 1.0))
+   painter))
+
+(paint (flip-horiz einstein))
+(paint (rotate180 einstein))
+(paint (rotate270 einstein))
