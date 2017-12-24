73cf88dea65964d4926766ca6a0a15411c93b373
Adrian Wong <adrianwjw@gmail.com>
2017-12-24 23:54:15 +1100

    add: 2.72.scm

---
+++ ch_2/2_3/2.72.scm
@@ -0,0 +1,5 @@
+; Most frequent symbol: O(1)
+
+; Least frequent symbol: O(n^2) worst-case, as we
+; have to search for the symbol in the sets of
+; symbols in the left and right branches at each level
