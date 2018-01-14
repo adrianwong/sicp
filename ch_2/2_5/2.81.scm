0a12b185e6241bb12fdf19efd0d00f12f741ee68
Adrian Wong <adrianwjw@gmail.com>
2018-01-14 23:07:06 +1100

    add: 2.81.scm

---
+++ ch_2/2_5/2.81.scm
@@ -0,0 +1,32 @@
+; (a)
+; If we call `exp` with two arguments of type `scheme-number`,
+; Louis' coercion procedure does nothing - the procedure is
+; never called. If we call `exp` with two arguments of type
+; `complex`, the `apply-generic` procedure recurses infinitely
+
+; (b)
+; No, Louis is wrong. `apply-generic` works correctly as-is
+
+; (c)
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags))
+          (no-method (error "No method for these types"
+                            (list op type-tags))))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (if (equal? type1 type2)
+                    no-method
+                    (let ((t1->t2 (get-coercion type1 type2))
+                          (t2->t1 (get-coercion type2 type1)))
+                      (cond (t1->t2
+                             (apply-generic op (t1->t2 a1) a2))
+                            (t2->t1
+                             (apply-generic op a1 (t2->t1 a2)))
+                            (else no-method)))))
+              no-method)))))
