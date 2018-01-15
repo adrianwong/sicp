e8be45615706cc272c6d7a1710b641daf04189e1
Adrian Wong <adrianwjw@gmail.com>
2018-01-15 17:45:50 +1100

    fix: Evaluation of `let` causes error to always be thrown

--- ch_2/2_5/2.81.scm
+++ ch_2/2_5/2.81.scm
@@ -11,8 +11,8 @@
 (define (apply-generic op . args)
   (let ((type-tags (map type-tag args)))
     (let ((proc (get op type-tags))
-          (no-method (error "No method for these types"
-                            (list op type-tags))))
+          (no-method (lambda () (error "No method for these types"
+                                       (list op type-tags)))))
       (if proc
           (apply proc (map contents args))
           (if (= (length args) 2)
@@ -21,12 +21,22 @@
                     (a1 (car args))
                     (a2 (cadr args)))
                 (if (equal? type1 type2)
-                    no-method
+                    (no-method)
                     (let ((t1->t2 (get-coercion type1 type2))
                           (t2->t1 (get-coercion type2 type1)))
                       (cond (t1->t2
                              (apply-generic op (t1->t2 a1) a2))
                             (t2->t1
                              (apply-generic op a1 (t2->t1 a2)))
-                            (else no-method)))))
-              no-method)))))
+                            (else (no-method))))))
+              (no-method))))))
+
+(install-scheme-number-package)
+
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
+(define (add x y) (apply-generic 'add x y))
+(define (exp x y) (apply-generic 'exp x y)) ; Doesn't exist in (install-scheme-number-package)
+
+(add (make-scheme-number 2) (make-scheme-number 4))
+(exp (make-scheme-number 2) (make-scheme-number 4)) ; Should fail
