2dc61c29ff39b108e613d0fb658058296b04d2c7
Adrian Wong <adrianwjw@gmail.com>
2018-01-18 21:35:02 +1100

    add: 2.86.scm

---
+++ ch_2/2_5/2.86.scm
@@ -0,0 +1,8 @@
+; Provide generic operations `sine`, `cosine`, `arctan`, `square`
+; and `sqrt`. Implement these operations for `scheme-number`,
+; `rational`, and any other numbers we wish to add to the system.
+;
+; Then, modify the `rectangular` and `polar` packages to make use
+; of these generic operations. Also, replace the primitive arithmetic
+; operations `+`, `-`, `*` and `/` in the `rectangular`, `polar`
+; and `complex` packages with their generic operation equivalents
