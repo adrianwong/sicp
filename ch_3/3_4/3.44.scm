315a4fc27b963be1da0499e6aa1b0cbe74906d2c
Adrian Wong <adrianwjw@gmail.com>
2018-02-27 00:14:41 +1100

    add: 3.44.scm

---
+++ ch_3/3_4/3.44.scm
@@ -0,0 +1,4 @@
+; Ben is correct. `transfer` avoids the issue of computing
+; `difference`, which may produce inconsistencies if two
+; `exchange` operations are interleaved (as demonstrated in
+; ex. 3.43)
