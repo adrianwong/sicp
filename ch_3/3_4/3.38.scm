17275a9f7a1981518fe200a0fc18e2c149d1c5d6
Adrian Wong <adrianwjw@gmail.com>
2018-02-26 22:47:25 +1100

    add: 3.38.scm

---
+++ ch_3/3_4/3.38.scm
@@ -0,0 +1,22 @@
+; (a)
+; Peter, Paul, Mary
+; 45
+
+; Peter, Mary, Paul
+; 35
+
+; Paul, Peter, Mary
+; 45
+
+; Paul, Mary, Peter
+; 50
+
+; Mary, Peter, Paul
+; 40
+
+; Mary, Paul, Peter
+; 40
+
+
+; (b)
+; Many possibilities
