7b3c71fcadadf36ea7abb7568f0fb4e7076c4799
Adrian Wong <adrianwjw@gmail.com>
2018-04-04 22:58:07 +1000

    fix: Better explanation for 3.41.scm

--- ch_3/3_4/3.41.scm
+++ ch_3/3_4/3.41.scm
@@ -1,1 +1,5 @@
-; No. Accessing the balance is a single non-mutating operation
+; No, Ben's change does not make much of a difference. Two scenarios
+; that Ben's change prohibits is having another process access the
+;`balance` mid-deposit or mid-withdraw, but it can be argued that
+; if those transactions have yet to be completed, then the value of
+; `balance` is accurate at the point in time it was accessed
